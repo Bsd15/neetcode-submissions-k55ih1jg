6b1a449d0929c7c5c06449fea27b70e437ac2954
Bsd15 <39040503+Bsd15@users.noreply.github.com>
2026-08-03 13:47:12 +0530

    Add: string-encode-and-decode - submission-6

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-6.java
@@ -0,0 +1,31 @@
+class Solution {
+    private final static char seperator = '#';
+
+    public String encode(List<String> strs) {
+        StringBuilder sb = new StringBuilder();
+        for (String str : strs) {
+            sb.append(str.length()).append(seperator).append(str);
+        }
+
+        return sb.toString();
+    }
+
+    public List<String> decode(String str) {
+        List<String> result = new LinkedList<>();
+        int i = 0;
+        int currStrLen = 0;
+        while (i < str.length()) {
+            while (str.charAt(i) != seperator) {
+                char c = str.charAt(i);
+                currStrLen = 10 * currStrLen + (c - '0');
+                i++;
+            }
+
+            i++;
+            result.add(str.substring(i, i + currStrLen));
+            i += currStrLen;
+            currStrLen = 0;
+        }
+        return result;
+    }
+}
