40bfd88a7dc5e9f22f0adb745413eefbd56c14cb
Bsd15 <39040503+Bsd15@users.noreply.github.com>
2026-08-29 18:44:20 +0530

    Add: task-scheduling - submission-5

---
+++ Data Structures & Algorithms/task-scheduling/submission-5.java
@@ -0,0 +1,41 @@
+class Solution {
+    public int leastInterval(char[] tasks, int n) {
+        Deque<int[]> coolDownQueue = new ArrayDeque<>();
+
+        int[] tasksMap = new int[26];
+        for (char t : tasks) {
+            ++tasksMap[t - 'A'];
+        }
+
+        PriorityQueue<Integer> pq = new PriorityQueue<>((t1, t2) -> tasksMap[t2] - tasksMap[t1]);
+
+        for (int i = 0; i < 26; i++) {
+            if (tasksMap[i] != 0) {
+                pq.add(i);
+            }
+        }
+
+        int cycle = 0;
+        while (!pq.isEmpty() || !coolDownQueue.isEmpty()) {
+            if (pq.isEmpty() && !coolDownQueue.isEmpty()) {
+                cycle = coolDownQueue.getFirst()[1];
+            } else {
+                ++cycle;
+            }
+            if (!coolDownQueue.isEmpty()) {
+                int[] task = coolDownQueue.getFirst();
+                if (task[1] == cycle) {
+                    coolDownQueue.removeFirst();
+                    pq.add(task[0]);
+                }
+            }
+
+            int task = pq.poll();
+            if (tasksMap[task] > 1) {
+                --tasksMap[task];
+                coolDownQueue.addLast(new int[] {task, cycle + 1 + n});
+            }
+        }
+        return cycle;
+    }
+}
