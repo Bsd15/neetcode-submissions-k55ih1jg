8632f996df6ff13680f50520970d3be6f5fbe574
Bsd15 <39040503+Bsd15@users.noreply.github.com>
2026-09-02 14:04:44 +0530

    Add: reorganize-string - submission-4

---
+++ Data Structures & Algorithms/reorganize-string/submission-4.py
@@ -0,0 +1,31 @@
+class Solution:
+    def reorganizeString(self, s: str) -> str:
+        freq_count = {}
+        max_freq = 0
+        for c in s:
+            count = 1 + freq_count.get(c, 0)
+            freq_count[c] = count
+            max_freq = max(max_freq, count)
+        
+        if max_freq > (len(s) + 1) // 2:
+            return ""
+
+        max_heap = []
+        cool_down = None
+        res = ""
+        
+        for k,v in freq_count.items():
+            heapq.heappush_max(max_heap, [v, k])
+        
+        while max_heap:
+            next_char = heapq.heappop_max(max_heap)
+            res += next_char[1]
+
+            if cool_down:
+                heapq.heappush_max(max_heap, cool_down)
+                cool_down = None
+            
+            if next_char[0] > 1:
+                next_char[0] -= 1
+                cool_down = next_char
+        return res
