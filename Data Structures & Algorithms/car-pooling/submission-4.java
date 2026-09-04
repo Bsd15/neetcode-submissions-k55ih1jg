3a11ee73ccae8f6edb8f06fc03f5bf6f767e9557
Bsd15 <39040503+Bsd15@users.noreply.github.com>
2026-09-04 13:51:45 +0530

    Add: car-pooling - submission-4

---
+++ Data Structures & Algorithms/car-pooling/submission-4.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean carPooling(int[][] trips, int capacity) {
+        int start = Integer.MAX_VALUE;
+        int end = Integer.MIN_VALUE;
+        for (int[] trip : trips) {
+            start = Math.min(start, trip[1]);
+            end = Math.max(end, trip[2]);
+        }
+
+        int n = end - start + 1;
+        int[] drops = new int[n];
+        for (int[] trip : trips) {
+            drops[trip[1] - start] = drops[trip[1] - start] + trip[0];
+            drops[trip[2] - start] = drops[trip[2] - start] - trip[0];
+        }
+
+        int currCap = 0;
+        for (int i = 0; i < drops.length; i++) {
+            currCap = currCap + drops[i];
+            if (currCap > capacity) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
